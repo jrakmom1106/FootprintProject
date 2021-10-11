fa91c66bce223ec3545e48972152f779de6afd9c
yoon <2021541012@test.com>
2021-10-11 21:44:37 +0900

    kakao api 에서 검색기능 추가.

--- app/src/main/java/kr/ac/mjc/footprint/MapActivity.kt
+++ app/src/main/java/kr/ac/mjc/footprint/MapActivity.kt
@@ -4,7 +4,6 @@ package kr.ac.mjc.footprint
 
 
 import android.Manifest
-
 import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -13,58 +12,226 @@ import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import android.view.View
-import android.widget.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapReverseGeoCoder
 import net.daum.mf.map.api.MapView
 import net.daum.mf.map.api.MapView.CurrentLocationEventListener
-
-
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MapActivity() : AppCompatActivity(), CurrentLocationEventListener,
     MapReverseGeoCoder.ReverseGeoCodingResultListener {
+
+    private val listItems = arrayListOf<ListLayout>()   // 리사이클러 뷰 아이템
+    private val listAdapter = ListAdapter(listItems)    // 리사이클러 뷰 어댑터
+    private var pageNumber = 1      // 검색 페이지 번호
+    private var keyword = ""        // 검색 키워드
+
+    lateinit var rvList : RecyclerView
+    lateinit var btnSearch : Button
+    lateinit var btnPrevPage : Button
+    lateinit var btnNextPage : Button
+    lateinit var etSearchField : EditText
+    lateinit var tvPageNumber: TextView
+
     private var mMapView: MapView? = null
     var REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
         mMapView = findViewById<View>(R.id.map_view) as MapView
+
+        rvList = findViewById(R.id.rv_list)
+        btnSearch = findViewById(R.id.btn_search)
+        btnNextPage = findViewById(R.id.btn_nextPage)
+        btnPrevPage = findViewById(R.id.btn_prevPage)
+        etSearchField = findViewById(R.id.et_search_field)
+        tvPageNumber = findViewById(R.id.tv_pageNumber) //
+
+        rvList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        rvList.adapter = listAdapter
+
         //mMapView.setDaumMapApiKey(MapApiConst.DAUM_MAPS_ANDROID_APP_API_KEY);
+
+
+        listAdapter.setItemClickListener(object: ListAdapter.OnItemClickListener {
+            override fun onClick(v: View, position: Int) {
+                val mapPoint = MapPoint.mapPointWithGeoCoord(listItems[position].y, listItems[position].x)
+                mMapView!!.setMapCenterPointAndZoomLevel(mapPoint, 1, true)
+            }
+        })
+
+        // 검색 버튼
+        btnSearch.setOnClickListener {
+            keyword = etSearchField.text.toString()
+            pageNumber = 1
+            searchKeyword(keyword, pageNumber)
+        }
+
+        // 이전 페이지 버튼
+        btnPrevPage.setOnClickListener {
+            pageNumber--
+            tvPageNumber.text = pageNumber.toString()
+            searchKeyword(keyword, pageNumber)
+        }
+
+        // 다음 페이지 버튼
+        btnNextPage.setOnClickListener {
+            pageNumber++
+            tvPageNumber.text = pageNumber.toString()
+            searchKeyword(keyword, pageNumber)
+        }
+
+
         mMapView!!.setCurrentLocationEventListener(this)
+
+
+
+
+
+
+
+/*
+        val marker = MapPOIItem()
+        marker.apply {
+            itemName = "은행"   // 마커 이름
+            mapPoint = MapPoint.mapPointWithGeoCoord(126.922337873383, 37.6189517907872)   // 좌표
+            markerType = MapPOIItem.MarkerType.CustomImage          // 마커 모양 (커스텀)
+            customImageResourceId = R.drawable.makerme               // 커스텀 마커 이미지
+            selectedMarkerType = MapPOIItem.MarkerType.CustomImage  // 클릭 시 마커 모양 (커스텀)
+            customSelectedImageResourceId = R.drawable.makerme       // 클릭 시 커스텀 마커 이미지
+            isCustomImageAutoscale = false      // 커스텀 마커 이미지 크기 자동 조정
+            setCustomImageAnchor(0.5f, 1.0f)    // 마커 이미지 기준점
+        }
+        mMapView!!.addPOIItem(marker)
+        */
+
+
+
+
+
+        /*
+
+        val marker = MapPOIItem()
+        val mapPoint = MapPoint.mapPointWithGeoCoord(126.922337873383, 37.6189517907872)
+        marker.itemName = "Default Marker"
+        marker.tag = 0
+        marker.mapPoint = mapPoint
+        marker.markerType = MapPOIItem.MarkerType.BluePin // 기본으로 제공하는 BluePin 마커 모양.
+
+        marker.selectedMarkerType = MapPOIItem.MarkerType.RedPin // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+
+        mMapView!!.addPOIItem(marker)
+
+         */
+
+
         if (!checkLocationServicesStatus()) {
             showDialogForLocationServiceSetting()
         } else {
             checkRunTimePermission()
         }
+
     }
+
+
+    private fun searchKeyword(keyword: String, Page:Int) {
+        val retrofit = Retrofit.Builder()   // Retrofit 구성
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        val api = retrofit.create(KakaoAPI::class.java)   // 통신 인터페이스를 객체로 생성
+        val call = api.getSearchKeyword(API_KEY, keyword,Page)   // 검색 조건 입력
+
+        // API 서버에 요청
+        call.enqueue(object : Callback<ResultSearchKeyword> {
+            override fun onResponse(call: Call<ResultSearchKeyword>, response: Response<ResultSearchKeyword>) {
+                // 통신 성공
+                addItemsAndMarkers(response.body())
+            }
+
+            override fun onFailure(call: Call<ResultSearchKeyword>, t: Throwable) {
+                // 통신 실패
+                Log.w("LocalSearch", "통신 실패: ${t.message}")
+            }
+        })
+    }
+
+
+    // 검색 결과 처리 함수
+    private fun addItemsAndMarkers(searchResult: ResultSearchKeyword?) {
+        if (!searchResult?.documents.isNullOrEmpty()) {
+            // 검색 결과 있음
+            listItems.clear()                   // 리스트 초기화
+            mMapView!!.removeAllPOIItems() // 지도의 마커 모두 제거
+            for (document in searchResult!!.documents) {
+                // 결과를 리사이클러 뷰에 추가
+                val item = ListLayout(document.place_name,
+                        document.road_address_name,
+                        document.address_name,
+                        document.x.toDouble(),
+                        document.y.toDouble())
+                listItems.add(item)
+
+                // 지도에 마커 추가
+                val point = MapPOIItem()
+                point.apply {
+                    itemName = document.place_name
+                    mapPoint = MapPoint.mapPointWithGeoCoord(document.y.toDouble(),
+                            document.x.toDouble())
+                    markerType = MapPOIItem.MarkerType.BluePin
+                    selectedMarkerType = MapPOIItem.MarkerType.RedPin
+                }
+                mMapView!!.addPOIItem(point)
+            }
+            listAdapter.notifyDataSetChanged()
+
+            btnNextPage.isEnabled = !searchResult.meta.is_end // 페이지가 더 있을 경우 다음 버튼 활성화
+            btnPrevPage.isEnabled = pageNumber != 1             // 1페이지가 아닐 경우 이전 버튼 활성화
+
+        } else {
+            // 검색 결과 없음
+            Toast.makeText(this, "검색 결과가 없습니다", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 
     override fun onDestroy() {
         super.onDestroy()
         mMapView!!.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOff
-        mMapView!!.setShowCurrentLocationMarker(false)
+        mMapView!!.setShowCurrentLocationMarker(true)
     }
 
     override fun onCurrentLocationUpdate(
-        mapView: MapView,
-        currentLocation: MapPoint,
-        accuracyInMeters: Float
+            mapView: MapView,
+            currentLocation: MapPoint,
+            accuracyInMeters: Float
     ) {
         val mapPointGeo = currentLocation.mapPointGeoCoord
         Log.i(
-            MapActivity.Companion.LOG_TAG,
-            String.format(
-                "MapView onCurrentLocationUpdate (%f,%f) accuracy (%f)",
-                mapPointGeo.latitude,
-                mapPointGeo.longitude,
-                accuracyInMeters
-            )
+                MapActivity.Companion.LOG_TAG,
+                String.format(
+                        "MapView onCurrentLocationUpdate (%f,%f) accuracy (%f)",
+                        mapPointGeo.latitude,
+                        mapPointGeo.longitude,
+                        accuracyInMeters
+                )
         )
     }
 
@@ -88,9 +255,9 @@ class MapActivity() : AppCompatActivity(), CurrentLocationEventListener,
      * ActivityCompat.requestPermissions를 사용한 퍼미션 요청의 결과를 리턴받는 메소드입니다.
      */
     override fun onRequestPermissionsResult(
-        permsRequestCode: Int,
-        permissions: Array<String>,
-        grandResults: IntArray
+            permsRequestCode: Int,
+            permissions: Array<String>,
+            grandResults: IntArray
     ) {
         super.onRequestPermissionsResult(permsRequestCode, permissions, grandResults)
         if (permsRequestCode == MapActivity.Companion.PERMISSIONS_REQUEST_CODE && grandResults.size == REQUIRED_PERMISSIONS.size) {
@@ -114,21 +281,21 @@ class MapActivity() : AppCompatActivity(), CurrentLocationEventListener,
             } else {
                 // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있습니다.
                 if (ActivityCompat.shouldShowRequestPermissionRationale(
-                        this,
-                        REQUIRED_PERMISSIONS[0]
-                    )
+                                this,
+                                REQUIRED_PERMISSIONS[0]
+                        )
                 ) {
                     Toast.makeText(
-                        this,
-                        "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.",
-                        Toast.LENGTH_LONG
+                            this,
+                            "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.",
+                            Toast.LENGTH_LONG
                     ).show()
                     finish()
                 } else {
                     Toast.makeText(
-                        this,
-                        "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ",
-                        Toast.LENGTH_LONG
+                            this,
+                            "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ",
+                            Toast.LENGTH_LONG
                     ).show()
                 }
             }
@@ -140,8 +307,8 @@ class MapActivity() : AppCompatActivity(), CurrentLocationEventListener,
         //런타임 퍼미션 처리
         // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
         val hasFineLocationPermission = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_FINE_LOCATION
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
         )
         if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED) {
 
@@ -156,9 +323,9 @@ class MapActivity() : AppCompatActivity(), CurrentLocationEventListener,
 
             // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
             if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    this,
-                    REQUIRED_PERMISSIONS[0]
-                )
+                            this,
+                            REQUIRED_PERMISSIONS[0]
+                    )
             ) {
 
                 // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
@@ -166,15 +333,15 @@ class MapActivity() : AppCompatActivity(), CurrentLocationEventListener,
                     .show()
                 // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
                 ActivityCompat.requestPermissions(
-                    this, REQUIRED_PERMISSIONS,
-                    MapActivity.Companion.PERMISSIONS_REQUEST_CODE
+                        this, REQUIRED_PERMISSIONS,
+                        MapActivity.Companion.PERMISSIONS_REQUEST_CODE
                 )
             } else {
                 // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
                 // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
                 ActivityCompat.requestPermissions(
-                    this, REQUIRED_PERMISSIONS,
-                    MapActivity.Companion.PERMISSIONS_REQUEST_CODE
+                        this, REQUIRED_PERMISSIONS,
+                        MapActivity.Companion.PERMISSIONS_REQUEST_CODE
                 )
             }
         }
@@ -185,20 +352,20 @@ class MapActivity() : AppCompatActivity(), CurrentLocationEventListener,
         val builder = AlertDialog.Builder(this)
         builder.setTitle("위치 서비스 비활성화")
         builder.setMessage(
-            "앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
-                    + "위치 설정을 수정하실래요?"
+                "앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
+                        + "위치 설정을 수정하실래요?"
         )
         builder.setCancelable(true)
         builder.setPositiveButton("설정") { dialog, id ->
             val callGPSSettingIntent =
                 Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
             startActivityForResult(
-                callGPSSettingIntent,
-                MapActivity.Companion.GPS_ENABLE_REQUEST_CODE
+                    callGPSSettingIntent,
+                    MapActivity.Companion.GPS_ENABLE_REQUEST_CODE
             )
         }
         builder.setNegativeButton(
-            "취소"
+                "취소"
         ) { dialog, id -> dialog.cancel() }
         builder.create().show()
     }
@@ -228,8 +395,14 @@ class MapActivity() : AppCompatActivity(), CurrentLocationEventListener,
         private val LOG_TAG = "MainActivity"
         private val GPS_ENABLE_REQUEST_CODE = 2001
         private val PERMISSIONS_REQUEST_CODE = 100
+        const val BASE_URL = "https://dapi.kakao.com/"
+        const val API_KEY = "KakaoAK 715aa7ab55967eb2cf591a7771968270"  // REST API 키
+
     }
-}
+    }
+
+
+
 
 
 
