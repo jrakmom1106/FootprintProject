cb12b1b07e299bc237ebfdbf1e10c672cfe95282
Yoon <jrakmom971106@gmail.com>
2021-11-12 10:44:15 +0900

    광고 이미지, 광고 viewPager 용 파일 추가. HomeFragment.java 의 Firebase 연동 코드 작성. -> 진행중

---
+++ app/src/main/java/kr/ac/mjc/footprint/advFragment2.kt
@@ -0,0 +1,24 @@
+package kr.ac.mjc.footprint
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+
+class advFragment2 : Fragment() {
+
+    lateinit var imageIv:ImageView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //layout을 가져와서 view 로 리턴
+        var view = inflater.inflate(R.layout.frame_2, container, false)
+        imageIv = view.findViewById(R.id.imageView1)
+        return view
+
+    }
+}
